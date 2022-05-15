20a515b360e42fe1e4bd602f16b839c19d4dde28
ArthurCaldas <Arthurcaldas90@outlook.com>
2022-05-15 15:35:57 -0300

    version 1
    Finalizado digitação e operações

---
+++ main.js
@@ -0,0 +1,30 @@
+/* Inserir digito */
+function insert(num) {
+    var numero = document.getElementById('Resultado').innerHTML;
+    document.getElementById('Resultado').innerHTML =  numero + num;
+}
+
+/* Limpar digitos */
+
+function clean() {
+    document.getElementById('Resultado').innerHTML= "";
+}
+
+/* Apagar digito */
+
+function DEL(){
+    var resultado = document.getElementById('Resultado').innerHTML;
+    document.getElementById('Resultado').innerHTML = resultado.substring(0,resultado.length -1)
+}
+
+/* Faz a operação */
+
+function conta() 
+{
+    var resultado = document.getElementById('Resultado').innerHTML;
+    if(resultado) 
+    {
+        document.getElementById('Resultado').innerHTML = eval(resultado);
+    }
+    else alert ("erro")
+}
